565de099b2ca7e9e68ba54ac1c44fdecc9064cf5
Ričards Cacurs <rcacurs@gmail.com>
2016-04-16 16:14:46 +0300

    init

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I'm alive");
+
+        //Transform tr = GetComponent<Transform>();
+        //transform.Translate(10, 0, 0);
+        Debug.Log("new transfrom" + transform.position);
+	}
+	
+	// Update is called once per frame
+    float speed = 2f;
+	void FixedUpdate() {
+ //      float distance = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+ //       transform.Translate(0.1f, 0, 0);
+	}
+
+    void AndroidUpdate(string message) {
+        float amount= float.Parse(message);
+        transform.Translate(amount, 0, 0);
+
+        AndroidJavaClass myClass = new AndroidJavaClass("lv.edi.testing.UnityPlayerActivity");
+        AndroidJavaObject myObject = myClass.CallStatic<AndroidJavaObject>("instance");
+        myObject.Call("testMethod", new object[] { "Message massing to android works!" });
+       
+    }
+
+    void Test2(string message)
+    {
+        Debug.Log("Test2called" + message);
+    }
+}
---
+++ Assets/ObjectRotation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour {
+
+	// Use this for initialization
+    Quaternion initialRotation;
+	void Start () {
+        initialRotation = transform.rotation;
+ 
+        
+        if (this.gameObject.name == "mixamorig:RightUpLeg"){
+            transform.localRotation = Quaternion.Inverse(transform.parent.rotation)*initialRotation;
+            //transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 90.0f)*initialRotation;
+
+        }
+        Debug.Log("new rotation" + transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.name == "mixamorig:Hips")
+        {
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+            //transform.Rotate(0, 0, 90, Space.World);
+        }
+	}
+
+    void SetRotation(string quaternion){
+        string[] components = quaternion.Split(',');
+        Debug.Log("Setting rotation. date received " + quaternion);
+        float x = float.Parse(components[0]);
+        float y = float.Parse(components[1]);
+        float z = float.Parse(components[2]);
+        float w = float.Parse(components[3]);
+        Debug.Log("Parsed components: " + x + " " + " " + y + " " + z + " " + w);
+        transform.rotation.Set(x, y, z, w);
+        
+    }
+}
