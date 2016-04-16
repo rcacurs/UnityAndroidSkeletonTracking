ea13333fcbe6e75687a9090d6a10733cc6d6e119
Clover <Clovergruff@gmail.com>
2016-04-17 00:54:45 +0300

    All kinds of things - UI, camera swiping, scene changing etc.

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -7,24 +7,54 @@ public class GameCamera : MonoBehaviour
 	public float pitch;
 
 	private Quaternion rotation;
+	private Quaternion lookRot;
 	private Vector3 position;
 
 	public Transform target;
 
+	private const float stiffness = 15;
+	private const float distance = 3;
+
+	void Awake()
+	{
+		The.gameCamera = this;
+
+		rotation = Quaternion.Euler(-15, 180, 0);
+
+		yaw = 200;
+		pitch = 15;
+	}
+
 	void Update()
 	{
-
+		// Controls
+		Controls();
 
 		// Pitch limits
 		pitch = Mathf.Clamp(pitch, -15, 75);
 
 		// Assign values for rotation and position
-		Quaternion targRot = Quaternion.Euler(pitch, yaw, 0);
-		rotation = Quaternion.Lerp(rotation, targRot, 15 * Time.deltaTime);
+		lookRot = Quaternion.Lerp(lookRot, Quaternion.Euler(pitch, yaw, 0), stiffness * Time.deltaTime);
 
-		Vector3 offset = new Vector3(0,0,-3);
-		Vector3 targPos = target.position + rotation * offset;
-		position = Vector3.Lerp(position, targPos, 15 * Time.deltaTime);
+		position = target.position + lookRot * new Vector3(0, 0, -distance); ;
+		rotation = Quaternion.LookRotation(target.position - position, Vector3.up);
+	}
+
+	void Controls()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			pitch -= Input.GetAxis("Mouse Y") * Settings.swipeSensitivity * Time.deltaTime;
+			yaw += Input.GetAxis("Mouse X") * Settings.swipeSensitivity * Time.deltaTime;
+
+			//Cursor.lockState = CursorLockMode.Locked;
+			//Cursor.visible = false;
+		}
+		else
+		{
+			//Cursor.lockState = CursorLockMode.None;
+			//Cursor.visible = true;
+		}
 	}
 
 	void LateUpdate()
