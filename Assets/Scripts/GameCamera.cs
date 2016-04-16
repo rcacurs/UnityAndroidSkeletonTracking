bb4bf8a401bd92312d2decefe9751ee09fbae62d
Clover <Clovergruff@gmail.com>
2016-04-16 22:43:57 +0300

    Whoops, forgot non-versioned files >___< Also updated Gitignore

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour
+{
+	public float yaw;
+	public float pitch;
+
+	private Quaternion rotation;
+	private Vector3 position;
+
+	public Transform target;
+
+	void Update()
+	{
+
+
+		// Pitch limits
+		pitch = Mathf.Clamp(pitch, -15, 75);
+
+		// Assign values for rotation and position
+		Quaternion targRot = Quaternion.Euler(pitch, yaw, 0);
+		rotation = Quaternion.Lerp(rotation, targRot, 15 * Time.deltaTime);
+
+		Vector3 offset = new Vector3(0,0,-3);
+		Vector3 targPos = target.position + rotation * offset;
+		position = Vector3.Lerp(position, targPos, 15 * Time.deltaTime);
+	}
+
+	void LateUpdate()
+	{
+		transform.rotation = rotation;
+		transform.position = position;
+	}
+}
---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour {
+
+	// Use this for initialization
+    Quaternion initialRotation;
+    Quaternion externalRotation = Quaternion.Euler(0, 0, 0);
+
+    
+	void Start() {
+        initialRotation = transform.rotation;
+        Debug.Log("Quaternion" + externalRotation);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        
+	}
+    void LateUpdate(){
+        transform.rotation = externalRotation*initialRotation;
+    }
+
+    void SetRotation(string quaternion){
+        string[] components = quaternion.Split(',');
+        Debug.Log("Setting rotation. date received " + quaternion);
+        float x = float.Parse(components[0]);
+        float y = float.Parse(components[1]);
+        float z = float.Parse(components[2]);
+        float w = float.Parse(components[3]);
+        Debug.Log("Parsed components: " + x + " " + " " + y + " " + z + " " + w);
+        
+        externalRotation = new Quaternion(y, z, w, x);
+        
+    }
+}
