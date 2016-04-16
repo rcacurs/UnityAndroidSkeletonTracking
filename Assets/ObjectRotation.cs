85a358a3017ddf3de641db1172d0861b85488918
Ričards Cacurs <rcacurs@gmail.com>
2016-04-16 17:35:00 +0300

    adding scripts to used body parts

--- Assets/ObjectRotation.cs
+++ Assets/ObjectRotation.cs
@@ -5,26 +5,21 @@ public class ObjectRotation : MonoBehaviour {
 
 	// Use this for initialization
     Quaternion initialRotation;
-	void Start () {
-        initialRotation = transform.rotation;
- 
-        
-        if (this.gameObject.name == "mixamorig:RightUpLeg"){
-            transform.localRotation = Quaternion.Inverse(transform.parent.rotation)*initialRotation;
-            //transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 90.0f)*initialRotation;
+    Quaternion externalRotation = Quaternion.Euler(0, 0, 0);
 
-        }
-        Debug.Log("new rotation" + transform.rotation);
+    
+	void Start() {
+        initialRotation = transform.rotation;
+        Debug.Log("Quaternion" + externalRotation);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (this.gameObject.name == "mixamorig:Hips")
-        {
-            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
-            //transform.Rotate(0, 0, 90, Space.World);
-        }
+	void Update() {
+        
 	}
+    void LateUpdate(){
+        transform.rotation = externalRotation*initialRotation;
+    }
 
     void SetRotation(string quaternion){
         string[] components = quaternion.Split(',');
@@ -34,7 +29,8 @@ public class ObjectRotation : MonoBehaviour {
         float z = float.Parse(components[2]);
         float w = float.Parse(components[3]);
         Debug.Log("Parsed components: " + x + " " + " " + y + " " + z + " " + w);
-        transform.rotation.Set(x, y, z, w);
+        
+        externalRotation = new Quaternion(y, z, w, x);
         
     }
 }
